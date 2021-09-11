d8c2b1e0352268fc18ca46a3603a13a11c1ed7d0
heyaaman - UB <90447061+Heyaaman-UB@users.noreply.github.com>
2021-09-11 20:57:43 +0530

    Update logomaker.py

--- innexiaBot/modules/logomaker.py
+++ innexiaBot/modules/logomaker.py
@@ -100,11 +100,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByInnexia.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="💕 Made By @SiderzBot Support @SiderzChat")
+    await tbot.send_file(event.chat_id, fname2, caption="💕 Made By @phoenix_empire Support @CFC_BOT_support")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @SiderzChat, {e}')
+   await event.reply(f'Error Report @CFC_BOT_support, {e}')
 
 
 
@@ -140,11 +140,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "LogoByInnexia.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @innexiaBot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @PhoenixWarmedbot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @SiderzChat, {e}')
+   await event.reply(f'Error Report @CFC_BOT_support, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
