6aeb251197fa8f1f204562473db8fc08fb31593f
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-09-22 10:15:48 +0530

    Update __google.py

--- innexiaBot/modules/__google.py
+++ innexiaBot/modules/__google.py
@@ -286,6 +286,6 @@ __help__ = """
  ❍ /github <username>*:* Get information about a GitHub user.
  ❍ /country <country name>*:* Gathering info about given country
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
- ❍ Innexia <query>*:* Innexia answers the query
-  💡Ex: `Innexia where is India?`
+ ❍ Phoenix <query>*:* Phoenix answers the query
+  💡Ex: `Phoenix where is India?`
 """
