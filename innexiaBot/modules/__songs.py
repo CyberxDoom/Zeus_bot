a7ac6ca16bfbfdef6697f6ec8a01e502b70912a7
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-09-24 23:09:10 +0530

    Update __songs.py

--- innexiaBot/modules/__songs.py
+++ innexiaBot/modules/__songs.py
@@ -36,14 +36,14 @@ async def song(client, message):
     os.remove(ffile)
     await m.delete()
 
-#Deezer
-
+#deezer#
+# Credits for @heyaaman
 
 import os
 import aiofiles
 import aiohttp
 from pyrogram import filters
-from innexiaBot import pbot as AMELIA
+from AmeliaRobot import pbot as AMELIA
 
 ARQ = "https://thearq.tech/"
 
@@ -89,3 +89,68 @@ async def deezer(_, message):
     await message.reply_audio(audio=song, title=title, performer=artist)
     os.remove(song)
     await m.delete()
+
+#Deezer
+# Credits for @TheHamkerCat
+
+import os
+import aiofiles
+import aiohttp
+from pyrogram import filters
+from innexiaBot import pbot as ASUNA
+
+ARQ = "https://thearq.tech/"
+
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            try:
+                data = await resp.json()
+            except:
+                data = await resp.text()
+    return data
+
+async def download_song(url):
+    song_name = f"asuna.mp3"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(song_name, mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    return song_name
+
+
+@innexia.on_message(filters.command("deezer"))
+async def deezer(_, message):
+    if len(message.command) < 2:
+        await message.reply_text("Download Now Deezer")
+        return
+    text = message.text.split(None, 1)[1]
+    query = text.replace(" ", "%20")
+    m = await message.reply_text("Searching...")
+    try:
+        r = await fetch(f"{ARQ}deezer?query={query}&count=1")
+        title = r[0]["title"]
+        url = r[0]["url"]
+        artist = r[0]["artist"]
+    except Exception as e:
+        await m.edit(str(e))
+        return
+    await m.edit("Downloading...")
+    song = await download_song(url)
+    await m.edit("Uploading...")
+    await message.reply_audio(audio=song, title=title, performer=artist)
+    os.remove(song)
+    await m.delete()
+    
+    
+__mod_name__ = "Music"
+
+__help__ = """
+• `/song`** <songname artist(optional)>: download the song in it's best quality available.(API BASED)
+• `/video`** <songname artist(optional)>: download the video song in it's best quality available.
+• `/deezer`** <songname>: download from deezer
+• `/lyrics`** <songname artist(optional)>: sends the complete lyrics of the song provided as input
+• `/glyrics`** <i> song name </i> : This plugin searches for song lyrics with song name and artist.
+"""
