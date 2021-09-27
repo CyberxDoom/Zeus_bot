e180e312105e54dbb9a3a620205f63e425872f27
heyaaman <85990282+heyaaman@users.noreply.github.com>
2021-09-27 13:15:53 +0530

    Update afk.py

--- innexiaBot/modules/afk.py
+++ innexiaBot/modules/afk.py
@@ -58,7 +58,7 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
+                "{} is back to alive!",
                 "{} is back!",
                 "{} is now in the chat!",
                 "{} is awake!",
